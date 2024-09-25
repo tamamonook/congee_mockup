8d04fec9815c64a350ec7ff53298eb6661163f38
Tamamo nook <tamamo@gmail.com>
2024-09-25 10:47:45 +0700

    My first push

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+def echo():
+    return jsonify({
+        "method": request.method,
+        "headers": dict(request.headers),
+        "args": request.args,
+        "form": request.form,
+        "json": request.json,
+        "data": request.data.decode("utf-8")
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
